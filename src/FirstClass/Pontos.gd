9007bb7f75d2aa65d66ac2da362e37b4a523f50a
Pedro Hagge Baptista <pepehaggeb@gmail.com>
2022-03-14 11:10:19 -0300

    adicionado novos temas para diálogos e pontos

--- src/FirstClass/Pontos.gd
+++ src/FirstClass/Pontos.gd
@@ -1,5 +1,6 @@
-extends Node
+extends Label
 
+export (Color, RGB) var text_color
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,6 +9,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_modulate(text_color)
 	pass # Replace with function body.
 
 
